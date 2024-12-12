676ab5213ebd7ecb6940ebeb95aff3941b7d1681
Parker Mortensen <parker.b.mortensen@gmail.com>
2024-12-11 18:43:38 -0700

    enforce secure connections in production

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
+// HTTPS middleware to enforce secure connections in production
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+  })
+}
+
 // frontend resources served from root directory for now
 app.use(express.static("."));
 
